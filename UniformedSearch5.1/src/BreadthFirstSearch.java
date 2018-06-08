a043c98b21f2eacd2d48af2ac42e9c570f63b8fb
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-06-08 09:07:41 -0400

    .

--- UniformedSearch5.1/src/BreadthFirstSearch.java
+++ UniformedSearch5.1/src/BreadthFirstSearch.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList; 
 import java.util.LinkedList;
-import java.util.Queue; 
+import java.util.Queue;
+
+import org.w3c.dom.Node; 
 
 public class BreadthFirstSearch {
 	
@@ -17,8 +19,34 @@ public class BreadthFirstSearch {
 	{
 		if (this.startNode.equals(goalNode))
 		{
-			
+			System.out.println("Goal Node Found!");
+			System.out.println(startNode);
 		}
+		
+		Queue<Node> queue = new LinkedList<>();
+		ArrayList<javax.xml.soap.Node> explored = new ArrayList<>();
+		queue.add(this.startNode);
+		explored.add(startNode);
+		
+		while(!queue.isEmpty())
+		{
+			Node current = queue.remove();
+			if(current.equals(this.goalNode)
+			{
+				System.out.println(explored);
+				return true;
+			}
+			else
+			{
+				if(current.getChildren().isEmpty())
+					return false;
+				else 
+					queue.addAll(current.getChildren());
+			}
+			explored.add(current);
+		}
+		
+		return false;
 	}
 
 }
